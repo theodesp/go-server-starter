9a1777db403443c4388ce0991839aa8257409710
theodesp <theo.despoudis@wpengine.com>
2020-05-16 21:58:49 +0100

    http server shutdown

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 )
 
@@ -19,8 +22,26 @@ func main() {
 		Handler:           mux,
 		Addr:              ":3000",
 	}
-	log.Println("Listening...")
-	server.ListenAndServe()
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt)
+		<-sigint
+
+		// We received an interrupt signal, shut down.
+		if err := server.Shutdown(context.Background()); err != nil {
+			// Error from closing listeners, or context timeout:
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+		close(idleConnsClosed)
+	}()
+
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		// Error starting or closing listener:
+		log.Fatalf("HTTP server ListenAndServe: %v", err)
+	}
+
+	<-idleConnsClosed
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
